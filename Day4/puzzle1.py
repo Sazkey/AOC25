f96d9d061438fd45e29684cf5fae75ae986234ba
Sazkey <Sazkeyy@gmail.com>
2025-12-15 22:04:58 +0100

    refactor vars

--- Day4/puzzle1.py
+++ Day4/puzzle1.py
@@ -2,15 +2,7 @@ with open("testInput.txt", "f") as file:
     data = file.read().replace("\n", " ")
     dataElements = data.split(" ")
 
-col_length = len(dataElements)
-row_length = len(dataElements[0])
-
-grid = [[0]* row_length] * col_length
-
-for i in range(col_length):
-    print("\n")
-    for j in range(row_length):
-        print(dataElements[j], end=" ")
-
+rows = len(dataElements)
+cols = len(dataElements[0])
 
 # def adjacent_paper(grid, indexi, indexj):
